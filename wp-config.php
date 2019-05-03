71e4ddc169c0c68f1032ec141661fcd16eba116d
Ryan Echter <ryan.echter@endurance.com>
2019-05-03 15:29:10 -0500

    Update variables for wp-config.

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', $_ENV['MYSQL_USER']);
 define( 'DB_PASSWORD', $_ENV['MYSQL_PASSWORD']);
 
 /** MySQL hostname */
-define( 'DB_HOST', $_ENV['uri']);
+define( 'DB_HOST', $_ENV['MYSQL_HOST']);
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         $_ENV['AUTH_KEY']);
+define( 'SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
+define( 'LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
+define( 'NONCE_KEY',        $_ENV['NONCE_KEY']);
+define( 'AUTH_SALT',        $_ENV['AUTH_SALT']);
+define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define( 'LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
+define( 'NONCE_SALT',       $_ENV['NONCE_SALT']);
 
 /**#@-*/
 
@@ -63,7 +63,7 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = $_ENV['DB_PREFIX'];
 
 /**
  * For developers: WordPress debugging mode.
